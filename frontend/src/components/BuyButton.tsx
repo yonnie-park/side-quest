5f42b845a3517cbc32f15b2272224840e784f306
yonnie-park <juon0805@gmail.com>
2026-02-24 15:28:49 +0900

    add faucet

--- frontend/src/components/BuyButton.tsx
+++ frontend/src/components/BuyButton.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './BuyButton.css';
+import React from "react";
+import "./BuyButton.css";
 
 interface BuyButtonProps {
   ticketCount: number;
@@ -10,11 +10,15 @@ interface BuyButtonProps {
 function BuyButton({ ticketCount, onClick, disabled }: BuyButtonProps) {
   return (
     <div className="buy-button-container">
-      <button 
-        className="buy-button"
-        onClick={onClick}
-        disabled={disabled}
+      <a
+        href="https://app.testnet.initia.xyz/faucet"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="faucet-link"
       >
+        ↗ INIT faucet
+      </a>
+      <button className="buy-button" onClick={onClick} disabled={disabled}>
         buy tickets ({ticketCount})
       </button>
     </div>
